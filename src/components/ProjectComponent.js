9883ab5a7638944eaefdc6bf82804fd2cb01a366
dsreliete <dsreliete@gmail.com>
2020-10-14 13:05:23 -0400

    ädded project component

---
+++ src/components/ProjectComponent.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+
+
+function Project() {
+    return(
+        <div className="contactus-container">
+            <div className="row">
+                <div className="col">
+                    <h1> Projeto blabla</h1>
+                    <h2>Wiskas sache</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Project;
