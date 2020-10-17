6be632f64fbd6e0eeae5b4ddab9f71ad96ba59a4
dsreliete <dsreliete@gmail.com>
2020-10-16 21:43:58 -0400

    ädded carrousel images to home

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,19 +1,43 @@
 import React  from 'react';
-import { Jumbotron } from 'reactstrap';
 
+import CaroulselImage from './CarouselImage';
+
+const items = [
+    {
+        src: '/assets/images/cozinha.JPG',
+        message: "COZINHA SOB MEDIDA",
+        key: '1'
+    },
+    {
+      src: '/assets/images/saladeestar.JPG',
+  	message: "SALA DE ESTAR",
+  	key: '2'
+    },
+    {
+        src: '/assets/images/dormitorios.JPG',
+        message: "DORMITÓRIO",
+        key: '3'
+    },
+    {
+        src: '/assets/images/banheiro.JPG',
+        message: "BANHEIRO",
+        key: '4'
+    },
+    {
+        src: '/assets/images/espacocomercial.JPG',
+        message: "SOLUÇÕES COMERCIAIS",
+        key: '5'
+    },
+    {
+        src: '/assets/images/espacocustomizado.JPG',
+        message: "ESPAÇOS CUSTOMIZADOS",
+        key: '6'
+    }
+];
 
 function Home(props) {
     return(
-    <Jumbotron fluid>
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <h1> {props.text} blabla</h1>
-                    <h2>Wiskas sache</h2>
-                </div>
-            </div>
-        </div>
-    </Jumbotron>
+        <CaroulselImage items={items} />
     );
 }
 
