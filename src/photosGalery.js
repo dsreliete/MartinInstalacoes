b521ca4e67d98a2e3ec0f0c3e03953eb57ed058a
dsreliete <dsreliete@gmail.com>
2020-11-06 16:26:29 -0500

    change photo galey ordem

--- src/photosGalery.js
+++ src/photosGalery.js
@@ -1,47 +1,51 @@
-export const galery = [
+export const photos = [
     {
       src: "/assets/images/portfolio/dormitorio/quartos1.JPG",
-      width: 2,
-      height: 4
+      width: 3,
+      height: 3,
+      title: 'DORMITÓRIOS'
+    },
+    {
+      src: "/assets/images/portfolio/cozinha/cozinhas10.JPG",
+      width: 4,
+      height: 4,
+      title: 'COZINHAS'
     },
     {
       src: "/assets/images/portfolio/lavanderia/lavanderias2.JPG",
-      width: 4,
-      height: 3
+      width: 3,
+      height: 2,
+      title: 'LAVANDERIAS'
     },
     {
       src: "/assets/images/portfolio/custom/custom13.JPG",
       width: 4,
-      height: 4
-    },
-    {
-      src: "/assets/images/portfolio/custom/custom25.JPG",
-      width: 2,
-      height: 4
+      height: 4,
+      title: 'AMBIENTES CUSTOMIZADOS'
     },
     {
       src: "/assets/images/portfolio/dormitorio/quartos14.JPG",
       width: 3,
-      height: 4
+      height: 4,
+      title: 'CLOSETS'
     },
     {
       src: "/assets/images/portfolio/escritorio/escritorios2.JPG",
       width: 3,
-      height: 3
-    },{
-      src: "/assets/images/portfolio/cozinha/cozinhas10.JPG",
-      width: 4,
-      height: 4
+      height: 3,
+      title: 'ESCRITÓRIOS'
     },
     {
       src: "/assets/images/portfolio/banheiro/banheiros1.JPG",
       width: 2,
-      height: 3
+      height: 3,
+      title: 'BANHEIROS'
     },
     {
       src: "/assets/images/portfolio/saladeestarjantar/salas2.JPG",
       width: 5,
-      height: 3
+      height: 3,
+      title: 'SALAS DE ESTAR / JANTAR'
     }
   ];
   
