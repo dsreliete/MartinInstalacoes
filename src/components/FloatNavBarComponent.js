6a52e7af30f98b36f6835e994cabc0dbc6f35910
dsreliete <dsreliete@gmail.com>
2020-11-10 22:51:52 -0500

    fix className warning

--- src/components/FloatNavBarComponent.js
+++ src/components/FloatNavBarComponent.js
@@ -5,9 +5,9 @@ const FloatNavBar = () => {
     return (
         <nav className="float-nav-bar">
             <ul>
-                <li><a href="https://www.facebook.com/djalma.camposmartin" target="_blank" rel="noopener noreferrer"><i class="fa fa-facebook"></i></a></li>
-                <li><a href="https://wa.me/5511998624973" target="_blank" rel="noopener noreferrer"><i class="fa fa-whatsapp"></i></a></li>
-                <li><a href="https://www.instagram.com/camposdjalma/" target="_blank" rel="noopener noreferrer"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/djalma.camposmartin" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a></li>
+                <li><a href="https://wa.me/5511998624973" target="_blank" rel="noopener noreferrer"><i className="fa fa-whatsapp"></i></a></li>
+                <li><a href="https://www.instagram.com/camposdjalma/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a></li>
             </ul>
         </nav>
     );
