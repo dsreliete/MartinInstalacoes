7df9e84aececab58bf75e2a5a95800d3dd1a2e4f
dsreliete <dsreliete@gmail.com>
2020-11-10 23:02:43 -0500

    fix bug on close lightbox

--- src/components/GallerySelectedImageComponent.js
+++ src/components/GallerySelectedImageComponent.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
-
 import Carousel, { Modal, ModalGateway } from "react-images";
 
 const SelectedImage = ({ 
     photo,
     index, 
     margin, 
-    selected,
     photosRoom,
     photosBath,
     photosBed,
@@ -14,120 +12,112 @@ const SelectedImage = ({
     photosDesk,
     photosKitchen,
     photosLaundry,
-  }) => {
+}) => {
 
-  const [isSelected, setIsSelected] = useState(selected);
-  const [viewerIsOpen, setViewerIsOpen] = useState(false);
-  const [imageCollection, setImageCollection] = useState([]);
+    const [isSelected, setIsSelected] = useState(false);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+    const [imageCollection, setImageCollection] = useState([]);
 
-  const onCloseLightbox = () => {
-    setViewerIsOpen(false);
-    setIsSelected(!isSelected);
-  };
+    const handleOnClick = () => {
+        const photoGalery = setPhotoGalery(index);
+        setImageCollection(photoGalery);
+        setViewerIsOpen(true);
+        setIsSelected(true);
+    };
 
-  const onOpenLightbox = ( ) => {
-    const photoGalery = setPhotoGalery(index);
-    setImageCollection(photoGalery);
-    
-    setViewerIsOpen(true);
-    setIsSelected(!isSelected);
-  };
+    const closeLightbox = () => {
+        setViewerIsOpen(false);
+        setIsSelected(false);
+    };
 
-  const setPhotoGalery = (index) => {
-    switch(index){
-        case 0:
-            return photosBed;
-        case 1:
-            return photosKitchen;
-        case 2:
-            return photosLaundry;
-        case 3:
-            return photosCustom;
-        case 4:
-            return photosBed;
-        case 5:
-            return photosDesk;
-        case 6:
-            return photosBath;
-        case 7:
-            return photosRoom;
-        default:
-            return photo;
+    const setPhotoGalery = (index) => {
+        switch(index){
+            case 0:
+                return photosBed;
+            case 1:
+                return photosKitchen;
+            case 2:
+                return photosLaundry;
+            case 3:
+                return photosCustom;
+            case 4:
+                return photosBed;
+            case 5:
+                return photosDesk;
+            case 6:
+                return photosBath;
+            case 7:
+                return photosRoom;
+            default:
+                return photo;
+        }
     }
-  }
 
-  return (
-    <div
-      style={{ margin, height: photo.height, width: photo.width }}
-      className={!isSelected ? "not-selected" : ""}
-      onClick={onOpenLightbox}>
-      
-      <img
-        alt="photos"
-        {...photo} />
+    return (
 
-      <div className="middle">
-        <div className="image-text">
-          {photo.title}
-        </div>
-      </div>
+        <div style={{ margin, height: photo.height, width: photo.width }}
+            className={!isSelected ? "not-selected" : ""}>
 
-      <style>{`
-        .not-selected:hover{
-          opacity:0.60; 
-          position: relative;
-        }
+            <img alt={photo.title} {...photo} onClick={handleOnClick}/>
+            <div className="middle">
+                <div className="image-text"
+                onClick={handleOnClick}>
+                {photo.title}
+                </div>
+            </div>
 
-        .not-selected:hover .middle {
-          opacity: 1;
-        }
+            <style>{`
+                .not-selected:hover{
+                    opacity:0.60; 
+                    position: relative;
+                }
 
-        .img {
-          opacity: 1;
-          display: block;
-          width: 100%;
-          height: auto;
-          transition: .1s ease in out;
-          backface-visibility: hidden;
-        }
-        
-        .middle {
-          transition: .1s ease in out;
-          opacity: 0;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
-          -ms-transform: translate(-50%, -50%);
-          text-align: center;
-        }
+                .not-selected:hover .middle {
+                    opacity: 1;
+                }
 
-        .image-text {
-          background-color: #009688;
-          color: white;
-          font-size: 16px;
-          padding: 16px 32px;
-          font-weight:bold
-        }
-      `}</style>
-      
-      <ModalGateway>
-          {
-            viewerIsOpen ? (
-              <Modal onClose={onCloseLightbox}>
-                  <Carousel
-                      views={imageCollection.map(x => ({
-                          ...x,
-                          srcset: x.srcSet,
-                      }))}
-                  />
-              </Modal>
-            ) 
-            : null
-          }
-      </ModalGateway>
-    </div>
-  );
+                .img {
+                    opacity: 1;
+                    display: block;
+                    width: 100%;
+                    height: auto;
+                    transition: .1s ease in out;
+                    backface-visibility: hidden;
+                    }
+                
+                .middle {
+                    transition: .1s ease in out;
+                    opacity: 0;
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    -ms-transform: translate(-50%, -50%);
+                    text-align: center;
+                }
+
+                .image-text {
+                    background-color: #009688;
+                    color: white;
+                    font-size: 16px;
+                    padding: 16px 32px;
+                    font-weight:bold
+                }
+            `}</style>
+
+        <ModalGateway>
+            { viewerIsOpen ? (
+                <Modal onClose={closeLightbox}>
+                    <Carousel
+                        views={imageCollection.map((x) => ({
+                        ...x,
+                        srcset: x.srcSet
+                        }))}
+                    />
+                </Modal>
+            ) : null }
+        </ModalGateway>
+    </div>);
 };
 
 export default SelectedImage;
